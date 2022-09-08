4e31d5225d3c56086412ddbe5f4e96c2290fbfb0
roddg86 <roddg@mail.ru>
2022-09-08 09:15:22 +0900

    references to objects

--- app/src/main/java/com/bignerdranch/android/tetris/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/tetris/MainActivity.kt
@@ -2,13 +2,23 @@ package com.bignerdranch.android.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var tvHighScore: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         /* скрыть панель действий */
         supportActionBar?.hide()
+
+        /* сформировать ссылки на обьекты для виджетов */
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+        tvHighScore = findViewById<TextView>(R.id.tv_high_score)
     }
 }
