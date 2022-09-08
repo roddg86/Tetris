9b6f7f2de8ded33df0ecb3ad6d03f37678b86a98
roddg86 <roddg@mail.ru>
2022-09-08 11:27:45 +0900

    game logic

--- app/src/main/java/com/bignerdranch/android/tetris/GameActivity.kt
+++ app/src/main/java/com/bignerdranch/android/tetris/GameActivity.kt
@@ -1,4 +1,39 @@
 package com.bignerdranch.android.tetris
 
-class GameActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.bignerdranch.android.tetris.storage.AppPreferences
+
+class GameActivity : AppCompatActivity() {
+    var tvHighScore: TextView? = null
+    var tvCurrentScore: TextView? = null
+    var appPreferences: AppPreferences? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+        appPreferences = AppPreferences(this)
+
+        /* создаем ссылки на обьекты элементов представлений макета */
+        val btnRestart = findViewById<Button>(R.id.btn_restart)
+        tvHighScore = findViewById<TextView>(R.id.tv_high_score)
+        tvCurrentScore = findViewById<TextView>(R.id.tv_current_score)
+
+        /* функции вызываются при создании представления */
+        updateHighScore()
+        updateCurrentScore()
+
+    }
+
+    /* функция максимального количества очков */
+    private fun updateHighScore(){
+        tvHighScore?.text = "${appPreferences?.getHighScore()}"
+    }
+
+    /* функция заданного по умолчанию количество очков */
+    private fun updateCurrentScore(){
+        tvCurrentScore?.text = "0"
+    }
 }
