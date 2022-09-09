0a4eaa229fa2f2e6babcc91419355ec6969c904a
roddg86 <roddg@mail.ru>
2022-09-09 17:24:33 +0900

    add tetris view

--- app/src/main/java/com/bignerdranch/android/tetris/GameActivity.kt
+++ app/src/main/java/com/bignerdranch/android/tetris/GameActivity.kt
@@ -2,38 +2,86 @@ package com.bignerdranch.android.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.bignerdranch.android.tetris.models.AppModel
 import com.bignerdranch.android.tetris.storage.AppPreferences
+import com.bignerdranch.android.tetris.view.TetrisView
 
-class GameActivity : AppCompatActivity() {
+class GameActivity: AppCompatActivity() {
+
     var tvHighScore: TextView? = null
     var tvCurrentScore: TextView? = null
-    var appPreferences: AppPreferences? = null
+    private lateinit var tetrisView: TetrisView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var appPreferences: AppPreferences? = null
+    private val appModel: AppModel = AppModel()
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         appPreferences = AppPreferences(this)
+        appModel.setPreferences(appPreferences)
 
-        /* создаем ссылки на обьекты элементов представлений макета */
         val btnRestart = findViewById<Button>(R.id.btn_restart)
         tvHighScore = findViewById<TextView>(R.id.tv_high_score)
         tvCurrentScore = findViewById<TextView>(R.id.tv_current_score)
+        tetrisView = findViewById<TetrisView>(R.id.view_tetris)
+        tetrisView.setActivity(this)
+        tetrisView.setModel(appModel)
 
-        /* функции вызываются при создании представления */
+        tetrisView.setOnTouchListener(this::onTetrisViewTouch)
+        btnRestart.setOnClickListener(this::btnRestartClick)
+
         updateHighScore()
         updateCurrentScore()
-
     }
 
-    /* функция максимального количества очков */
-    private fun updateHighScore(){
+    private fun btnRestartClick(view: View) {
+        appModel.restartGame()
+    }
+
+    private fun onTetrisViewTouch(view: View, event: MotionEvent): Boolean {
+        if (appModel.isGameOver() || appModel.isGameAwaitingStart()) {
+            appModel.startGame()
+            tetrisView.setGameCommandWithDelay(AppModel.Motions.DOWN)
+
+        } else if(appModel.isGameActive()) {
+            when (resolveTouchDirection(view, event)) {
+                0 -> moveTetromino(AppModel.Motions.LEFT)
+                1 -> moveTetromino(AppModel.Motions.ROTATE)
+                2 -> moveTetromino(AppModel.Motions.DOWN)
+                3 -> moveTetromino(AppModel.Motions.RIGHT)
+            }
+        }
+        return true
+    }
+
+    private fun resolveTouchDirection(view: View, event: MotionEvent): Int {
+        val x = event.x / view.width
+        val y = event.y / view.height
+        val direction: Int
+
+        direction = if (y > x) {
+            if (x > 1 - y) 2 else 0 } else {
+            if (x > 1 - y) 3 else 1
+        }
+        return direction
+    }
+
+    private fun moveTetromino(motion: AppModel.Motions) {
+        if (appModel.isGameActive()) {
+            tetrisView.setGameCommand(motion)
+        }
+    }
+
+    private fun updateHighScore() {
         tvHighScore?.text = "${appPreferences?.getHighScore()}"
     }
 
-    /* функция заданного по умолчанию количество очков */
-    private fun updateCurrentScore(){
+    private fun updateCurrentScore() {
         tvCurrentScore?.text = "0"
     }
 }
