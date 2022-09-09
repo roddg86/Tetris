71a9faae23827af624727a5dd90a1bc090b03962
roddg86 <roddg@mail.ru>
2022-09-09 16:43:21 +0900

    creating a model

--- app/src/main/java/com/bignerdranch/android/tetris/models/AppModel.kt
+++ app/src/main/java/com/bignerdranch/android/tetris/models/AppModel.kt
@@ -18,6 +18,89 @@ class AppModel {
         FieldConstants.COLUMN_COUNT.value
     )
 
+    fun generateField(action: String) {
+        if (isGameActive()) {
+            resetField()
+
+            var frameNumber: Int? = currentBlock?.frameNumber
+            val coordinate: Point? = Point()
+            coordinate?.x = currentBlock?.position?.x
+            coordinate?.y = currentBlock?.position?.y
+
+            when (action) {
+                Motions.LEFT.name -> {
+                    coordinate?.x = currentBlock?.position?.x?.minus(1)
+                }
+                Motions.RIGHT.name -> {
+                    coordinate?.x = currentBlock?.position?.x?.plus(1)
+                }
+                Motions.DOWN.name -> {
+                    coordinate?.y = currentBlock?.position?.y?.plus(1)
+                }
+                Motions.ROTATE.name -> {
+                    frameNumber = frameNumber?.plus(1)
+
+                    if (frameNumber != null) {
+                        if (frameNumber >= currentBlock?.frameCount as Int) {
+                            frameNumber = 0
+                        }
+                    }
+                }
+            }
+
+            if (!moveValid(coordinate as Point, frameNumber)) {
+                translateBlock(currentBlock?.position as Point, currentBlock?.frameNumber as Int)
+
+                if (Motions.DOWN.name == action) {
+                    boostScore()
+                    persistCellData()
+                    assessField()
+                    generateNextBlock()
+
+                    if (!blockAdditionPossible()) {
+                        currentState = Statuses.OVER.name
+                        currentBlock = null
+                        resetField(false)
+                    }
+                }
+
+            } else {
+                if (frameNumber != null) {
+                    translateBlock(coordinate, frameNumber)
+                    currentBlock?.setState(frameNumber, coordinate)
+                }
+            }
+        }
+    }
+
+    private fun resetField(ephemeralCellsOnly: Boolean = true) {
+        for (i in 0 until FieldConstants.ROW_COUNT.value) {
+            (0 until FieldConstants.COLUMN_COUNT.value)
+                .filter { !ephemeralCellsOnly || field[i][it] == CellConstants.EPHEMERAL.value }
+                .forEach { field[i][it] = CellConstants.EMPTY.value }
+        }
+    }
+
+    private fun translateBlock(position: Point, frameNumber: Int) {
+        synchronized(field) {
+            val shape: Array<ByteArray>? = currentBlock?.getShape(frameNumber)
+
+            if (shape != null) {
+                // All cell is correct - add the data:
+                for (i in shape.indices) {
+                    for (j in 0 until shape[i].size) {
+                        val y = position.y + i
+                        val x = position.x + j
+                        if (CellConstants.EMPTY.value != shape[i][j]) {
+                            field[y][x] = shape[i][j]
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
     private fun moveValid(position: Point, frameNumber: Int?): Boolean {
         val shape: Array<ByteArray>? = currentBlock?.getShape(frameNumber as Int)
         return validTranslation(position, shape as Array<ByteArray>)
@@ -47,6 +130,62 @@ class AppModel {
         }
     }
 
+    private fun blockAdditionPossible(): Boolean {
+        // Check the validity of new block:
+        if (!moveValid(currentBlock?.position as Point, currentBlock?.frameNumber)) {
+            // GAME IS OVER!
+            return false
+        }
+        return true
+    }
+
+    private fun generateNextBlock() {
+        currentBlock = Block.createBlock()
+    }
+
+    private fun persistCellData() {
+        // set all the dynamic data as static:
+        for (i in 0 until field.size) {
+            for (j in 0 until field[i].size) {
+                var status = getCellStatus(i, j)
+
+                if (status == CellConstants.EPHEMERAL.value) {
+                    status = currentBlock?.staticValue
+                    setCellStatus(i, j, status)
+                }
+            }
+        }
+    }
+
+    private fun assessField() {
+        for (i in 0 until field.size) {
+            var emptyCells = 0;
+
+            for (j in 0 until field[i].size) {
+                val status = getCellStatus(i, j)
+                val isEmpty = CellConstants.EMPTY.value == status
+
+                if (isEmpty)
+                    emptyCells++
+            }
+            if (emptyCells == 0)
+                shiftRows(i)
+        }
+    }
+
+    private fun shiftRows(nToRow: Int) {
+        if (nToRow > 0) {
+            for (j in nToRow - 1 downTo 0) {
+                for (m in 0 until field[j].size) {
+                    setCellStatus(j + 1, m, getCellStatus(j, m))
+                }
+            }
+        }
+        for (j in 0 until field[0].size) {
+            setCellStatus(0, j, CellConstants.EMPTY.value)
+        }
+    }
+
     fun setPreferences(preferences: AppPreferences?) {
         this.preferences = preferences
     }
@@ -60,6 +199,30 @@ class AppModel {
             field[row][column] = status
         }
     }
+
+    fun startGame() {
+        if (!isGameActive()) {
+            currentState = Statuses.ACTIVE.name
+            generateNextBlock()
+        }
+    }
+
+    fun restartGame() {
+        resetModel()
+        startGame()
+    }
+
+    fun endGame() {
+        score = 0
+        currentState = AppModel.Statuses.OVER.name
+    }
+
+    private fun resetModel() {
+        resetField(false)
+        currentState = Statuses.AWAITING_START.name
+        score = 0
+    }
+
 
     private fun boostScore() {
         score += 10
