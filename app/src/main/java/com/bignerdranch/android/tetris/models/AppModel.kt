539889a90ff54f4cd9c57a925735a7102089c486
roddg86 <roddg@mail.ru>
2022-09-09 15:46:56 +0900

    adding constants

--- app/src/main/java/com/bignerdranch/android/tetris/models/AppModel.kt
+++ app/src/main/java/com/bignerdranch/android/tetris/models/AppModel.kt
@@ -1,5 +1,11 @@
 package com.bignerdranch.android.tetris.models
 
 class AppModel {
+    enum class Statuses {
+        AWAITING_START, ACTIVE, OVER
+    }
 
+    enum class Motions {
+        LEFT, RIGHT, DOWN, ROTATE
+    }
 }
