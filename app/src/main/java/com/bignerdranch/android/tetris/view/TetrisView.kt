5e70a5623cc580cd4119314ffa7b26c0f8916f44
roddg86 <roddg@mail.ru>
2022-09-09 17:00:30 +0900

    implementation of the view

--- app/src/main/java/com/bignerdranch/android/tetris/view/TetrisView.kt
+++ app/src/main/java/com/bignerdranch/android/tetris/view/TetrisView.kt
@@ -1,12 +1,21 @@
 package com.bignerdranch.android.tetris.view
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
+import android.os.Handler
+import android.os.Message
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
 import androidx.annotation.Dimension
 import com.bignerdranch.android.tetris.GameActivity
+import com.bignerdranch.android.tetris.constants.CellConstants
+import com.bignerdranch.android.tetris.constants.FieldConstants
 import com.bignerdranch.android.tetris.models.AppModel
+import com.bignerdranch.android.tetris.models.Block
 
 class TetrisView : View {
 
@@ -22,10 +31,129 @@ class TetrisView : View {
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
 
+    fun setModel(model: AppModel) {
+        this.model = model
+    }
+
+    fun setActivity(gameActivity: GameActivity) {
+        this.activity = gameActivity
+    }
+
+    fun setGameCommand(move: AppModel.Motions) {
+        if (null != model && (model?.currentState == AppModel.Statuses.ACTIVE.name)) {
+            if (AppModel.Motions.DOWN == move) {
+                model?.generateField(move.name)
+                invalidate()
+                return
+            }
+            setGameCommandWithDelay(move)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawFrame(canvas)
+
+        if (model != null) {
+            for (i in 0 until FieldConstants.ROW_COUNT.value) {
+                for (j in 0 until FieldConstants.COLUMN_COUNT.value) {
+                    drawCell(canvas, i, j)
+                }
+            }
+        }
+    }
+
+    private fun drawCell(canvas: Canvas, row: Int, col: Int) {
+        val cellStatus = model?.getCellStatus(row, col)
+
+        if (CellConstants.EMPTY.value != cellStatus) {
+            val color = if (CellConstants.EPHEMERAL.value == cellStatus) {
+                model?.currentBlock?.color
+            } else {
+                Block.getColor(cellStatus as Byte)
+            }
+            drawCell(canvas, col, row, color as Int)
+        }
+    }
+
+    private fun drawCell(canvas: Canvas, x: Int, y: Int, rgbColor: Int) {
+        paint.color = rgbColor
+
+        val top: Float = (frameOffset.height + y * cellSize.height + BLOCK_OFFSET).toFloat()
+        val left: Float = (frameOffset.width + x * cellSize.width + BLOCK_OFFSET).toFloat()
+        val bottom: Float = (frameOffset.height + (y + 1) * cellSize.height - BLOCK_OFFSET).toFloat()
+        val right: Float = (frameOffset.width + (x + 1) * cellSize.width - BLOCK_OFFSET).toFloat()
+        val rectangle = RectF(left, top, right, bottom)
+
+        canvas.drawRoundRect(rectangle, 4F, 4F, paint)
+    }
+
+    private fun drawFrame(canvas: Canvas) {
+        paint.color = Color.LTGRAY
+
+        canvas.drawRect(frameOffset.width.toFloat(),
+            frameOffset.height.toFloat(), width - frameOffset.width.toFloat(),
+            height - frameOffset.height.toFloat(), paint)
+    }
+
+    override fun onSizeChanged(width: Int, height: Int, previousWidth: Int, previousHeight: Int) {
+        super.onSizeChanged(width, height, previousWidth, previousHeight)
+
+        val cellWidth = (width - 2 * FRAME_OFFSET_BASE) / FieldConstants.COLUMN_COUNT.value
+        val cellHeight = (height - 2 * FRAME_OFFSET_BASE) / FieldConstants.ROW_COUNT.value
+        val n = Math.min(cellWidth, cellHeight)
+        this.cellSize = Dimension(n, n)
+        val offsetX = (width - FieldConstants.COLUMN_COUNT.value * n) / 2
+        val offsetY = (height - FieldConstants.ROW_COUNT.value * n) / 2
+        this.frameOffset = Dimension(offsetX, offsetY)
+    }
+
+    fun setGameCommandWithDelay(move: AppModel.Motions) {
+        val now = System.currentTimeMillis()
+
+        if (now - lastMove > DELAY) {
+            model?.generateField(move.name)
+            invalidate()
+            lastMove = now
+        }
+        updateScores()
+        viewHandler.sleep(DELAY.toLong())
+    }
+
+    private fun updateScores() {
+        activity?.tvCurrentScore?.text = "${model?.score}"
+        activity?.tvHighScore?.text = "${activity?.appPreferences?.getHighScore()}"
+    }
+
+    private class ViewHandler(private val owner: TetrisView) : Handler() {
+
+        override fun handleMessage(message: Message) {
+            if (message.what == 0) {
+                if (owner.model != null) {
+                    if (owner.model!!.isGameOver()) {
+                        owner.model?.endGame()
+                        Toast.makeText(owner.activity, "Game over", Toast.LENGTH_LONG).show()
+                        //owner.activity!!.endGame()
+                    }
+                    if (owner.model!!.isGameActive()) {
+                        owner.setGameCommandWithDelay(AppModel.Motions.DOWN)
+                    }
+                }
+            }
+        }
+
+        fun sleep(delay: Long) {
+            this.removeMessages(0)
+            sendMessageDelayed(obtainMessage(0), delay)
+        }
+    }
+
     companion object {
         private val DELAY = 500
         private val BLOCK_OFFSET = 2
         private val FRAME_OFFSET_BASE = 10
     }
+
+    private data class Dimension(val width: Int, val height: Int)
 
 }
